8e9c6c5553b7faee03313ffbf6bea0eb65a5209d
Rajesh Malviya <rajeshmalviya1710@gmail.com>
2021-05-25 19:01:13 +0530

    prettier ESLint added

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -6,7 +6,7 @@ import {
   TableCell,
 } from '@material-ui/core';
 
-export default function Datatable({ data }) {
+export default function Datatable({ data }){
   const columns = data[0] && Object.keys(data[0]);
   return (
     <Table border="1" cellPadding={5} cellSpacing={1}>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import { DataGrid } from '@material-ui/data-grid';
-// import { useDemoData } from '@material-ui/x-grid-data-generator';
 
-export default function SizePaginationGrid({data}) {
-//   const { data } = useDemoData({
-//     dataSet: 'Commodity',
-//     rowLength: 100,
-//     maxColumns: 6,
-//   });
+export default function SizePaginationGrid(tabledata) {
+console.log(tabledata);
 
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGrid pageSize={5} rowsPerPageOptions={[5, 10, 20]} pagination {...data} />
+      <DataGrid pageSize={5} rowsPerPageOptions={[5, 10, 20]} pagination {...tabledata} />
     </div>
   );
 }
